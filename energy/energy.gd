e94965b074f973ac4b49f6a125c3c0518c3ac917
Chadfire <chadfire10@yahoo.com>
2026-03-07 19:14:28 -0500

    Working on energy generation

---
+++ energy/energy.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+@export var speed: float = 400.0
+@export var lifetime: float = 8.0
+
+var velocity: Vector2 = Vector2.ZERO
+
+func _ready() -> void:
+	pass
+
+
+func _physics_process(delta: float) -> void:
+	position += velocity * delta
+
+func launch(direction: Vector2) -> void:
+	velocity = direction.normalized() * speed
